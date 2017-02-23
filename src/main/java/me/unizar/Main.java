db0f03bd97237a76a6b76f713fe1f6d38780fe0c
AnaximandroAbe <anaximandroabernathy@gmail.com>
2017-02-23 21:55:57 +0100

    wiiiiii

--- src/main/java/me/unizar/Main.java
+++ src/main/java/me/unizar/Main.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		run("kittens");
+		//run("kittens");
 		//run("me_at_the_zoo");
 		//run("trending_today");
-		//run("videos_worth_spreading");
+		run("videos_worth_spreading");
 	}
 	
 	public static void run(String file) throws IOException{
